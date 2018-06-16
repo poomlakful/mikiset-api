fe5dfc91041f1cfe151e07e66e0352de9ca7a33c
Poom Tammawutikul <poomlak1994@hotmail.com>
2018-06-17 01:53:48 +0700

    config for run on appengine

--- src/main/java/com/poomdev/mikisetapi/MikisetApiApplication.java
+++ src/main/java/com/poomdev/mikisetapi/MikisetApiApplication.java
@@ -2,11 +2,18 @@ package com.poomdev.mikisetapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ImportResource("classpath:camel-context.xml")
-public class MikisetApiApplication {
+public class MikisetApiApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MikisetApiApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MikisetApiApplication.class, args);
